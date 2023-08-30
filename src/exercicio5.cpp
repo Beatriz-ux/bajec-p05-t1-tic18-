43009b87c8630232b03520fb4e851c43fed49270
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-08-30 12:01:45 -0300

    feat: iniciando exercicio 5

--- src/exercicio5.cpp
+++ src/exercicio5.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 int main(void){
+    unsigned char info_gene;
+    unsigned short int gene;
+
+    cout << "Digite o valor do gene: ";
+    cin >> info_gene;
+
+
+
+
+    cout << "Qual gene quer verificar? (1-8)\n";
+    cin >> gene;
+    
+    gene = 1 << (gene - 1);
+
+    gene = info_gene & gene;
+
+    gene > 0 ? cout << "Gene presente" : cout << "Gene não presente";
 
 
     return 0;
