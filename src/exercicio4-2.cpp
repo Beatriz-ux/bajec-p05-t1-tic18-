cdbd663858236c49e3dca4bb5fe231c44b220213
João Manoel <jmaoliveira.cic@uesc.br>
2023-08-31 10:23:59 -0300

    feat: exercicios 4-1, 4-2 e 5

--- src/exercicio4-2.cpp
+++ src/exercicio4-2.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(void){
+    //a)
+    double a, b, c, x, delta, x1, x2;
 
+    cout << "Digite os valores dos coeficientes a, b e c de um polinomio de segundo grau" << endl;
+    cin >> a >> b >> c;
+
+    delta = pow(b, 2) - (4 * a * c);
+
+    delta < 0 ? cout << "O polinomio nao possui raizes reais" << endl : delta == 0 ? cout << "O polinomio possui uma raiz real" << endl : cout << "O polinomio possui duas raizes reais" << endl;
+
+    delta >= 0 ? x1 = (-b + sqrt(delta)) / (2 * a) : x1 = 0;
+    delta >= 0 ? x2 = (-b - sqrt(delta)) / (2 * a) : x2 = 0;
+
+    delta == 0 ?  cout << "A raiz do polinomio eh: " << x1 << endl : delta > 0 ?   cout << "As raizes do polinomio sao: " << x1 << " e " << x2 << endl : cout << "O polinomio nao possui raizes reais" << endl;
+
+    //b)
+    cout << "Digite um valor para x" << endl;
+    cin >> x;
+
+    cout << "O valor de p(x) eh: " << (a * pow(x, 2)) + (b * x) + c << endl;
 
     return 0;
 }
--- src/exercicio5.cpp
+++ src/exercicio5.cpp
@@ -5,12 +5,29 @@ using namespace std;
 int main(void){
     unsigned char info_gene;
     unsigned short int gene;
+    int cont = 0;
 
     cout << "Digite o valor do gene: ";
     cin >> info_gene;
 
+    gene = info_gene & 1;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 2;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 4;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 8;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 16;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 32;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 64;
+    gene > 0 ? cont++ : cont = cont;
+    gene = info_gene & 128;
+    gene > 0 ? cont++ : cont = cont;
 
-
+    cout << cont << " genes estao presentes" << endl;
 
     cout << "Qual gene quer verificar? (1-8)\n";
     cin >> gene;
