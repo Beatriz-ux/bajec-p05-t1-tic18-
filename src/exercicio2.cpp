4fa45375898d3a2607665bb985f6594cd8d12dd6
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-31 22:48:16 -0300

    feat: item d

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -9,5 +9,15 @@ int main(){
     cout << ((ch1>=65 && ch1<=90)? "Caractere Maisculo":
             (ch1>=97 && ch1<=122)? "Caractere Minusculo":
             (ch1>=48 && ch1<=57)? "Caractere Digito": "Outro tipo de caractere");
+    
+    // isdigit(caractere) retorna nao-zero se o caracter for um numero
+    // islower(caractere) retorna nao-zero se o caracter for uma letra minuscula
+    // isupper(caractere) retorna nao-zero se o caracter for uma letra maiuscula
+    //
+    // (!isdigit() || !islower() || !isupper(caractere)) poderia ser usado para 
+    // saber se é algum outro tipo de caracte
+
+    
+
     return 0;
 }
