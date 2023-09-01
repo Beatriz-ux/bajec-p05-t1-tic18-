6ee40e87813fa9f220c4a4dd1bb79209a7909cfb
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-31 22:55:40 -0300

    feat: item e, f

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -17,7 +17,16 @@ int main(){
     // (!isdigit() || !islower() || !isupper(caractere)) poderia ser usado para 
     // saber se é algum outro tipo de caracte
 
-    
+    ch2 = 81;
+    cout << "\n\nO caractere: " << ch2 << " | (dec): " << (int) ch2;
+    cout << " | (oct): " << oct << (int) ch2 << " | (hex): " << hex <<(int) ch2;
+    cout << dec << endl;
+
+    ch3 = tolower(ch2);
+    cout << "\n\nO caractere: " << ch3 << " | (dec): " << (int) ch3;
+    cout << " | (oct): " << oct << (int) ch3 << " | (hex): " << hex <<(int) ch3;
+    cout << dec << endl;
+
 
     return 0;
 }
