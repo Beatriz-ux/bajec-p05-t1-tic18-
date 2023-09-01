08f2c9742102e2a6b0f7eff182f6eb0d9302dcce
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-09-01 09:38:26 -0300

    fix: eficiencia exercicio 5

--- src/exercicio5.cpp
+++ src/exercicio5.cpp
@@ -4,40 +4,48 @@ using namespace std;
 
 int main(void){
     unsigned char info_gene;
+    unsigned char gene_teste = 0b1;
     unsigned short int gene;
     int cont = 0;
 
-    cout << "Digite o valor do gene: ";
-    cin >> info_gene;
+    cout << "Entre com a informacao genetica da planta: " << endl;
+    cin >> gene;
+    
+    info_gene = (unsigned char) gene;
 
-    gene = info_gene & 1;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 2;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 4;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 8;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 16;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 32;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 64;
-    gene > 0 ? cont++ : cont = cont;
-    gene = info_gene & 128;
-    gene > 0 ? cont++ : cont = cont;
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+
+    info_gene & gene_teste ? cont++ : cont = cont;
+    gene_teste = gene_teste << 1;
+    
+    info_gene & gene_teste ? cont++ : cont = cont;
 
     cout << cont << " genes estao presentes" << endl;
 
     cout << "Qual gene quer verificar? (1-8)\n";
     cin >> gene;
     
-    gene = 1 << (gene - 1);
+    gene = 0b1 << (gene - 1);
 
     gene = info_gene & gene;
 
     gene > 0 ? cout << "Gene presente" : cout << "Gene não presente";
-
 
     return 0;
 }
